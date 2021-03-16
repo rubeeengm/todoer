c909b4e6c2249d044407ffb504951b78a6098b61
rubeeengm <rubeeengm@outlook.com>
2021-03-15 18:30:29 -0600

    begin

---
+++ todo/__init__.py
@@ -0,0 +1,20 @@
+import os
+
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config.from_mapping(
+        SECRET_KEY = 'mikey'
+        , DATABASE_HOST = os.environ.get('FLASK_DATABASE_HOST')
+        , DATABASE_PASSWORD = os.environ.get('FLASK_DATABASE_PASSWORD')
+        , DATABASE_USER = os.environ.get('FLASK_DATABASE_USER')
+        , DATABASE = os.environ.get('FLASK_DATABASE')
+    )
+
+    @app.route('/hola')
+    def hola():
+        return 'Hola mundo'
+
+    return app
